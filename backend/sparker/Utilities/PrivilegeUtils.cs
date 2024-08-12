9bc8dfc961769de574f56cdebb907b1fc738b33b
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-12 18:14:24 +0200

    Made utility class for checking privileges

---
+++ backend/sparker/Utilities/PrivilegeUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace sparker.Utilities
+{
+    using Microsoft.EntityFrameworkCore;
+    using sparker.Database;
+    using System.Threading.Tasks;
+
+    public static class PrivilegeUtils
+    {
+        // The DB context is included as an agrgument
+        public static async Task<bool> IsUserAdmin(ApplicationDbContext context, int userId)
+        {
+            return await context.Admins.AnyAsync(a => a.User_Id == userId);
+        }
+
+        // The DB context is included as an agrgument
+        public static async Task<bool> IsUserMasterAdmin(ApplicationDbContext context, int userId)
+        {
+            return await context.Admins
+                .AnyAsync(a => a.User_Id == userId && a.Is_Master);
+        }
+    }
+}
