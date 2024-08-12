284a94baf4e5b5ee890a8e659b49506eec3449da
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-12 18:09:28 +0200

    Added IsAdmin to LoginResponseDTO

---
+++ backend/sparker/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace sparker.DTOs
+{
+    public class LoginResponseDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Token { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
