7157b9ef6fbc75e9c240fa7858cf82bbf80d39b3
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-14 09:42:18 +0200

    Added firstname to the Profile page title

--- frontend/sparker/src/components/ProfilePage.tsx
+++ frontend/sparker/src/components/ProfilePage.tsx
@@ -1,23 +1,24 @@
 // ProfilePage.tsx
 import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from '@chakra-ui/react';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState, useContext, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import '../styles/ProfilePage.css';
 import '../styles/Global.css';
-
+import UserType from '../interfaces/UserInterface';
+import { AuthContext } from '../context/AuthContext';
 
 const ProfilePage = () => {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
     const cancelRef = React.useRef()
-  
+    const { user } = useContext(AuthContext);
 
 
     return (
         <div className="global-container">
 
             <div className='page-title'>
-             <h1>Profile Page</h1>
+             <h1>Profile Page - {user?.firstName}</h1>
              </div>
             <nav>
                 <div>
