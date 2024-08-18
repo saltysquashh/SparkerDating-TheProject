2686c4014de1689a7b81973a1a1047491f815b70
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-18 16:31:24 +0200

    Created new DTO for SwipeDetails

---
+++ backend/sparker/DTOs/SwipeDetailsDTO.cs
@@ -0,0 +1,13 @@
+﻿using sparker.Models;
+
+namespace sparker.DTOs
+{
+    public class SwipeDetailsDTO
+    {
+        // the details of the user that was swiped
+
+        public ShowcaseUserDTO ShowcaseUserDTO { get; set; }
+        public Swipe Swipe { get; set; } // the details of the swipe
+        public Match Match { get; set; } // the match (if it exists)
+    }
+}
