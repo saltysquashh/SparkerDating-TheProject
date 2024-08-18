6032e2a17eeaa8d83b411c7d7eed27781f396563
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-18 16:32:35 +0200

    Added additional info/details to the swipe details page

--- frontend/sparker/src/components/SwipeDetailsPage.tsx
+++ frontend/sparker/src/components/SwipeDetailsPage.tsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect, useContext } from 'react';
 import '../styles/SwipeDetailsPage.css';
 import { fetch_ShowcaseUser } from '../services/userService';
-import { deleteSwipe } from '../services/swipeService';
+import { deleteSwipe, fetchSwipeDetails } from '../services/swipeService';
+import { fetchMatch } from '../services/matchService';
 import UserType from '../interfaces/UserInterface';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from '@chakra-ui/react';
 import { AuthContext } from '../context/AuthContext';
+import SwipeHistoryType from '../interfaces/SwipeHistoryInterface';
+import SwipeType from '../interfaces/SwipeInterface';
 
 const SwipeDetailsPage = () => {
     const { swipeId } = useParams();
     const { swipeUserId } = useParams();
     const [swipeUserInfo, setUserInfo] = useState<UserType | null>(null);
+    const [swipe, setSwipe] = useState<SwipeType | null>(null);
     const [images, setImages] = useState<string[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isMatched, setIsMatched] = useState(false); // Track if a match exists
     const navigate = useNavigate();
     const { user } = useContext(AuthContext);
 
@@ -24,9 +29,17 @@ const SwipeDetailsPage = () => {
         const loadData = async () => {
             setIsLoading(true);
             try {
-                const response = await fetch_ShowcaseUser(swipeUserId);
-                setUserInfo(response);
-                setImages(response.images || []);
+                const swipeDetails = await fetchSwipeDetails (swipeUserId, user?.id);
+                setUserInfo(swipeDetails.showcaseUserDTO);
+                setImages(swipeDetails.showcaseUserDTO.images || []);
+                setSwipe(swipeDetails.swipe);
+
+                // Check if a match exists between the user and swipeUser
+                
+                const matchExists = !!swipeDetails.match;
+
+                setIsMatched(matchExists); // Set if a match exists
+
             } catch (error) {
                 console.error('Error fetching swipe data:', error);
             }
@@ -54,13 +67,22 @@ const SwipeDetailsPage = () => {
         navigate(`/swipehistory/`);
     };
 
+    const handleGoToMatchClick = () => {
+        navigate(`/matches/match/${swipeId}/${swipeUserId}`);
+    };
+
     return (
+        <div className="global-container">
+        <div className="swipe-details-page-container">
+        <div className="swipe-page-title">
+                <h2>Swipe</h2>
+            </div>
         <div className="swipe-card-container">
             <div className="swipe-header">
+                <div className="swipe-name-section">
                 <h2>{swipeUserInfo.firstName} {swipeUserInfo.lastName}</h2>
-                <p className="swipe-bio">{swipeUserInfo.bio}</p>
-            </div>
-            <div className="swipe-images-container">
+                </div>
+                <div className="swipe-images-container">
                 {images.length > 0 ? (
                     images.map((image, index) => (
                         <div key={index} className="swipe-image-thumbnail">
@@ -72,11 +94,44 @@ const SwipeDetailsPage = () => {
                         <img src="/images/default-user-image.png" alt="Default" className="swipe-item-image" />
                     </div>
                 )}
+                </div>
+                <div className="swipe-bio-section">
+                <div className="swipe-bio-title">Bio </div>
+                <div className="swipe-bio-content">
+                    <p>{swipeUserInfo.bio}</p>
+                </div>
+                </div>
+                <div className="swipe-details-section">
+                <div className="swipe-details-title">
+                    <h1>Details</h1>
+                </div>
+                <div className="swipe-detail-text">Age: {swipeUserInfo?.age}</div>
+                <div className="swipe-detail-text">Gender: {swipeUserInfo?.gender}</div>
+                </div> 
             </div>
+                
+                <div className="swipe-status-section">
+                    <div className="swipe-status-title">
+                    <h1>Status</h1>
+                    </div>
+                    <div className="swipe-status-text">You swiped at:</div>
+                    <div>{swipe?.swiped_At}</div>
+                    {!isMatched ? (
+                            <h1>You have not matched with {swipeUserInfo.firstName} yet</h1>
+                        ) : (
+                            <p>You matched with {swipeUserInfo.firstName}</p>
+                        )}
+                    </div>
             <div className="swipe-actions">
-                <Button onClick={onOpen} colorScheme='red' className="action-button">Undo swipe</Button>
+                        {!isMatched ? (
+                            <Button onClick={onOpen} colorScheme='red' className="action-button">Undo swipe</Button>
+                        ) : (
+                            <Button onClick={handleGoToMatchClick} colorScheme='blue' className="action-button">Go to Match</Button>
+                        )}
+                    </div>
             </div>
-
+            </div>
+            
             <AlertDialog
                 isOpen={isOpen}
                 leastDestructiveRef={cancelRef}
