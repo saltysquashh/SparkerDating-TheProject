df1a4b72d9eab85f4c5b3f4f68117255172d7fbc
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-24 19:26:25 +0200

    Added MatchUserDTO to the MatchDTO

--- backend/sparker/DTOs/MatchDTO.cs
+++ backend/sparker/DTOs/MatchDTO.cs
@@ -2,12 +2,13 @@
 {
     public class MatchDTO
     {
-        public int MatchId { get; set; }
+        public int Id { get; set; }
         public int User1Id { get; set; }
         public int User2Id { get; set; }
         public DateTime? LastMessageUser1 { get; set; }
         public DateTime? LastMessageUser2 { get; set; }
         public DateTime? MatchedAt { get; set; }
         public bool IsGhosted { get; set; }
+        public MatchUserDTO? MatchUser { get; set; }
     }
 }
