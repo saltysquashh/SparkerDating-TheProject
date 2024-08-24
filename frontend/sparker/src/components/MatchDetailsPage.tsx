a85af4eb7363937d969b8027a8e76dfdd464b636
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-24 19:29:52 +0200

    Optimized the MatchDetailsPage by reducing the amount of API calls needed, and changed the time-left functionality

--- frontend/sparker/src/components/MatchDetailsPage.tsx
+++ frontend/sparker/src/components/MatchDetailsPage.tsx
@@ -1,70 +1,73 @@
 import React, { useState, useEffect, useContext } from 'react';
 import '../styles/MatchDetailsPage.css';
-import { fetch_ShowcaseUser } from '../services/userService';
-import { fetchMatch, deleteMatch } from '../services/matchService';
-import UserType from '../interfaces/UserInterface';
-import MatchType from '../interfaces/MatchInterface';
+import { deleteMatch, getMatchById } from '../services/matchService';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from '@chakra-ui/react';
 import { AuthContext } from '../context/AuthContext';
 import { calculateTimeLeft, formatDate } from '../utilities/dateUtils';
+import MatchType from '../interfaces/MatchInterface';
 
 const MatchDetailsPage = () => {
     const { matchId, matchUserId } = useParams();
-    const [matchUserInfo, setMatchUserInfo] = useState<UserType | null>(null);
-    const [images, setImages] = useState<string[]>([]);
-
-    const [timeLeftUser1, setTimeLeftUser1] = useState<{ hours: number; minutes: number; seconds: number } | null>(null);
-    const [timeLeftUser2, setTimeLeftUser2] = useState<{ hours: number; minutes: number; seconds: number } | null>(null);
-    const [matchInfo, setMatchInfo] = useState<MatchType | null>(null);
+    const [match, setMatch] = useState<MatchType | null>(null);
+    const [timeLeftUser1, setTimeLeftUser1] = useState<string | null>(null);
+    const [timeLeftUser2, setTimeLeftUser2] = useState<string | null>(null);
     const [createdAt, setCreatedAt] = useState<string | null>(null);
     const [isGhosted, setIsGhosted] = useState(false);
     const navigate = useNavigate();
     const { user } = useContext(AuthContext);
     const userId = user?.id;
-
     const { isOpen, onOpen, onClose } = useDisclosure();
     const cancelRef = React.useRef<HTMLButtonElement>(null);
 
+    const updateTimers = (matchData: MatchType) => {
+        const { timeLeftUser1, timeLeftUser2 } = calculateTimeLeft(matchData, userId, matchData.matchUser?.id);
+        
+        setTimeLeftUser1(formatTimeLeft(timeLeftUser1));
+        setTimeLeftUser2(formatTimeLeft(timeLeftUser2));
+    };
+
+    const formatTimeLeft = (timeLeft: { hours: number; minutes: number; seconds: number } | null): string => {
+        if (!timeLeft || (timeLeft.hours <= 0 && timeLeft.minutes <= 0 && timeLeft.seconds <= 0)) {
+            return "Any minute...";
+        }
+
+        const { hours, minutes, seconds } = timeLeft;
+        return `${hours}h ${minutes}m ${seconds}s`;
+    };
+
     useEffect(() => {
         const loadData = async () => {
-          
             try {
-                const showcaseUser = await fetch_ShowcaseUser(matchUserId);
-                setMatchUserInfo(showcaseUser);
-                setImages(showcaseUser.images || []);
-
-                const RetrievedMatchData = await fetchMatch(matchId);
-                setMatchInfo(RetrievedMatchData);
+                const RetrievedMatchData = await getMatchById(matchId, userId);
+                setMatch(RetrievedMatchData);
                 setCreatedAt(formatDate(RetrievedMatchData.matchedAt));
-
-                const { timeLeftUser1, timeLeftUser2 } = calculateTimeLeft(RetrievedMatchData, user?.id, matchUserInfo?.id);
-                setTimeLeftUser1(timeLeftUser1);
-                setTimeLeftUser2(timeLeftUser2);
-
+                updateTimers(RetrievedMatchData);
                 setIsGhosted(RetrievedMatchData.isGhosted);
             } catch (error) {
                 console.error('Error fetching match data:', error);
             }
-            
         };
 
-        loadData();
+        const intervalLoadData = setInterval(loadData, 45000);
 
-        const intervalId = setInterval(() => {
-            if (matchInfo) {
-                const { timeLeftUser1, timeLeftUser2 } = calculateTimeLeft(matchInfo, user?.id, matchUserInfo?.id);
-                setTimeLeftUser1(timeLeftUser1);
-                setTimeLeftUser2(timeLeftUser2);
+        loadData();  // Initial load
+
+        return () => clearInterval(intervalLoadData);
+    }, [matchId, userId]);
+
+    useEffect(() => {
+        const intervalTimeLeft = setInterval(() => {
+            if (match) {
+                updateTimers(match);
             }
         }, 1000);
 
-        return () => clearInterval(intervalId);
-    }, [matchUserId, matchInfo, user, matchUserInfo]);
+        return () => clearInterval(intervalTimeLeft);
+    }, [match]);
 
-
-    if (!matchUserInfo) {
+    if (!match || !match.matchUser) {
         return <div className="match-page-error">Match not found.</div>;
     }
 
@@ -90,11 +93,11 @@ const MatchDetailsPage = () => {
                 <div className="match-card-container">
                     <div className="match-header">
                         <div className="match-name-section">
-                            <h2>{matchUserInfo.firstName} {matchUserInfo.lastName}</h2>
+                            <h2>{match.matchUser.fullName}</h2>
                         </div>
                         <div className="match-images-container">
-                            {images.length > 0 ? (
-                                images.map((image, index) => (
+                            {match.matchUser.images.length > 0 ? (
+                                match.matchUser.images.map((image, index) => (
                                     <div key={index} className="match-image-thumbnail">
                                         <img src={`data:image/png;base64,${image}`} alt="Match" />
                                     </div>
@@ -108,37 +111,31 @@ const MatchDetailsPage = () => {
                         <div className="match-bio-section">
                             <div className="match-bio-title">Bio</div>
                             <div className="match-bio-content">
-                                <p>{matchUserInfo.bio}</p>
+                                <p>{match.matchUser.bio}</p>
                             </div>
                         </div>
                         <div className="match-details-section">
                             <div className="match-details-title">
                                 <h1>Details</h1>
                             </div>
-                            <div className="match-detail-text">Age: {matchUserInfo?.age}</div>
-                            <div className="match-detail-text">Gender: {matchUserInfo?.gender}</div>
+                            <div className="match-detail-text">Age: {match.matchUser.age}</div>
+                            <div className="match-detail-text">Gender: {match.matchUser.gender}</div>
                         </div>
                     </div>
 
                     <div className="match-status-section">
                         <div className="match-status-title">
-                            <h1>Status</h1>
+                            <h1>Ghosting Status</h1>
                         </div>
-                        <div className="match-status-text">Matched at: {createdAt}</div>
-                        {timeLeftUser1 && (
-                            <p>
-                                {userId === matchUserId ? matchUserInfo.firstName : user?.firstName}:
-                                {` Time left before ghosting: ${timeLeftUser1.hours}h ${timeLeftUser1.minutes}m ${timeLeftUser1.seconds}s`}
-                            </p>
-                        )}
-                        {timeLeftUser2 && (
-                            <p>
-                                {userId === matchUserId ? user?.firstName : matchUserInfo.firstName}:
-                                {` Time left before ghosting: ${timeLeftUser2.hours}h ${timeLeftUser2.minutes}m ${timeLeftUser2.seconds}s`}
-                            </p>
-                        )}
-                    </div>
 
+                        <div className="time-left-title">Time left before ghosting:</div>
+                        <p>
+                            {userId === match.user1Id ? "You" : match.matchUser.fullName}: {timeLeftUser1}
+                        </p>
+                        <p>
+                            {userId === match.user2Id ? "You" : match.matchUser.fullName}: {timeLeftUser2}
+                        </p>
+                    </div>
                     <div className="match-actions">
                         {!isGhosted ? (
                             <Button onClick={handleChatClick} colorScheme='blue' className="action-button">Chat</Button>
@@ -171,9 +168,10 @@ const MatchDetailsPage = () => {
                             </AlertDialogContent>
                         </AlertDialogOverlay>
                     </AlertDialog>
+                    <div className="matched-at-text">You matched with {match.matchUser.fullName} on {createdAt}</div>
                 </div>
             </div>
-        </div> 
+        </div>
     );
 };
 
