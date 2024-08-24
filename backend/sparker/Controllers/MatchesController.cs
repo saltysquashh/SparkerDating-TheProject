6bf949f7fe633398cce046871f8d191531be89a2
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-24 19:26:06 +0200

    Changed the MatchesController endpoints

--- backend/sparker/Controllers/MatchesController.cs
+++ backend/sparker/Controllers/MatchesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileSystemGlobbing;
 using sparker.Database;
 using sparker.DTOs;
 using sparker.Models;
@@ -23,32 +24,57 @@ namespace sparker.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet("matchbyid/{id}")]
-        public async Task<IActionResult> GetMatch(int id)
+        // get a single match by its ID, and by the ID of the client-user it's fetched for
+        [HttpGet("matchbyid/{matchId}/byuserid/{userId}")]
+        public async Task<IActionResult> GetMatch(int matchId, int userId)
         {
-            var match = await _context.Matches.FindAsync(id);
+            var match = await _context.Matches.FindAsync(matchId);
 
             if (match == null)
             {
-                return NotFound($"Match with ID {id} not found.");
+                return NotFound($"Match with ID {matchId} not found.");
             }
+
+            var matchUserId = match.User1_Id == userId ? match.User2_Id : match.User1_Id;
+            var matchUser = await _context.Users.FindAsync(matchUserId);
+
+            if (matchUser == null)
+            {
+                return NotFound($"User with ID {matchUserId} not found.");
+            }
+
+            // Fetch user images and convert to Base64 string
+            var userImages = await _context.Images
+                                .Where(i => i.User_Id == matchUserId)
+                                .Select(i => Convert.ToBase64String(i.Image_Data))
+                                .ToListAsync();
 
             var matchDTO = new MatchDTO
             {
-                MatchId = match.Id,
+                Id = match.Id,
                 User1Id = match.User1_Id,
                 User2Id = match.User2_Id,
+                LastMessageUser1 = await DateUtils.LastUserMsg(_context, match.Id, match.User1_Id),
+                LastMessageUser2 = await DateUtils.LastUserMsg(_context, match.Id, match.User2_Id),
                 MatchedAt = match.Matched_At,
                 IsGhosted = match.Is_Ghosted,
-                LastMessageUser1 = await DateUtils.LastUserMsg(_context, match.Id, match.User1_Id),
-                LastMessageUser2 = await DateUtils.LastUserMsg(_context, match.Id, match.User2_Id)
-        };
+
+                MatchUser = new MatchUserDTO
+                {
+                    Id = matchUserId,
+                    FullName = matchUser.First_Name + " " + matchUser.Last_Name,
+                    Gender = matchUser.Gender,
+                    Age = DateUtils.CalculateAge(matchUser.Birthdate),
+                    Bio = matchUser.Bio,
+                    Images = userImages
+                }
+            };
 
             return Ok(matchDTO);
         }
 
-        // get all the matches the userid appears in
-        [HttpGet("matchesbyuserid/{userId}")]
+        // get ALL the matches the userid appears in
+        [HttpGet("getallmatchesbyuserid/{userId}")]
         public async Task<IActionResult> GetMatches(int userId)
         {
             var matches = await _context.Matches
@@ -60,32 +86,49 @@ namespace sparker.Controllers
 
             // Fetch user images and convert to Base64 string
             var userImages = await _context.Images
-                .Where(i => userIDs.Contains(i.User_Id))
-                .GroupBy(i => i.User_Id)
-                .Select(g => new { UserId = g.Key, ImageData = g.FirstOrDefault().Image_Data }) // Assuming there's an ImageData property
-                .ToDictionaryAsync(g => g.UserId, g => Convert.ToBase64String(g.ImageData));
+                            .Where(i => userIDs.Contains(i.User_Id))
+                            .GroupBy(i => i.User_Id)
+                            .Select(g => new { UserId = g.Key, ImageData = g.FirstOrDefault().Image_Data })
+                            .ToDictionaryAsync(g => g.UserId, g => Convert.ToBase64String(g.ImageData));
 
-            var matchUserDTOs = matches.Select(m =>
+            var matchDTOs = new List<MatchDTO>();
+
+            foreach (var match in matches)
             {
-                var matchedUserId = m.User1_Id == userId ? m.User2_Id : m.User1_Id;
-                var matchedUser = users[matchedUserId];
+                var matchUserId = match.User1_Id == userId ? match.User2_Id : match.User1_Id;
+                var matchUser = users[matchUserId];
 
-                return new MatchUserDTO
+                var matchDTO = new MatchDTO
                 {
-                    MatchId = m.Id,
-                    MatchedAt = m.Matched_At,
-                    MatchedUserId = matchedUserId,
-                    MatchedName = matchedUser.First_Name + " " + matchedUser.Last_Name,
-                    MatchedUserBio = matchedUser.Bio,
-                    MatchedImageData = userImages.ContainsKey(matchedUserId) ? userImages[matchedUserId] : null, // Set Base64 image data
-                    MatchIsGhosted = m.Is_Ghosted
+                    Id = match.Id,
+                    User1Id = match.User1_Id,
+                    User2Id = match.User2_Id,
+                    LastMessageUser1 = await DateUtils.LastUserMsg(_context, match.Id, match.User1_Id),
+                    LastMessageUser2 = await DateUtils.LastUserMsg(_context, match.Id, match.User2_Id),
+                    MatchedAt = match.Matched_At,
+                    IsGhosted = match.Is_Ghosted,
+                    MatchUser = new MatchUserDTO
+                    {
+                        Id = matchUserId,
+                        FullName = matchUser.First_Name + " " + matchUser.Last_Name,
+                        Gender = matchUser.Gender,
+                        Age = DateUtils.CalculateAge(matchUser.Birthdate),
+                        Bio = matchUser.Bio,
+                        Images = await _context.Images
+                                    .Where(image => image.User_Id == matchUserId)
+                                    .Select(image => Convert.ToBase64String(image.Image_Data))
+                                    .ToListAsync()
+                    }
                 };
-            });
 
-            return Ok(matchUserDTOs);
+                matchDTOs.Add(matchDTO);
+            }
+
+            return Ok(matchDTOs);
         }
 
 
+        // delete match
         [HttpDelete("delete/{matchId}/{userId}")]
         public async Task<IActionResult> DeleteMatch(int matchId, int userId)
         {
