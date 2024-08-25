254b64bc1ce628025484ef7d5ca25da33ed054ab
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-25 18:56:07 +0200

    Created UserActivitySummaryDTO for returning New and expired matches

---
+++ backend/sparker/DTOs/UserActivitySummaryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace sparker.DTOs
+{
+    public class UserActivitySummaryDTO
+    {
+        public List<MatchDTO> NewMatches { get; set; }
+        public List<MatchDTO> ExpiredMatches { get; set; }
+    }
+}
