b52f75e2c7d7dc7b46e5a9231c767753bb8d99ea
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-26 19:36:26 +0200

    Removed blank line in RegisterDTO

--- backend/sparker/DTOs/RegisterDTO.cs
+++ backend/sparker/DTOs/RegisterDTO.cs
@@ -4,7 +4,6 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        // public string Gender { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime Birthdate { get; set; }
