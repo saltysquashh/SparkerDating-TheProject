c174bbde3c00490e8746c26a76a2f6b0b764ee66
Joachim Breus Bock <joachimbock35@gmail.com>
2024-08-26 12:31:36 +0200

    Added Ghosted at to Match table

--- backend/sparker/Models/Match.cs
+++ backend/sparker/Models/Match.cs
@@ -12,5 +12,6 @@ namespace sparker.Models
         public int User2_Id { get; set; }
         public DateTime Matched_At { get; set; }
         public bool Is_Ghosted { get; set; } = false;
+        public DateTime? Ghosted_At { get; set; } 
     }
 }
